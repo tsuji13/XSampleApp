d92ed5c26870760c1ab2485d9669b41aada8f189
tsujimura <tsujimura@takachiho-sc.co.jp>
2026-04-09 14:05:23 +0900

    260409-3 下記セルパーツとHomeTableViewCellのIBOutlet接続を実施 ・ユーザー名を表示するuserNameLabel ・ユーザーアイコンを表示するuserImageView ・ポスト本文を表示するbodyLabel

---
+++ XSampleApp/HomeTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  HomeTableViewCell.swift
+//  XSampleApp
+//
+//  Created by kaihatsu on 2026/04/07.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    //ユーザーアイコンのImageView
+    @IBOutlet weak var userImageView: UIImageView!
+    
+    //ユーザー名のLabel
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    //ポスト本文のLabel
+    @IBOutlet weak var bodyLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
