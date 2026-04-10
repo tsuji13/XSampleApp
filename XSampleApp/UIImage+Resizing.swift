7ccd6c610bd615abe364ee7533388a56eb5f28ed
tsujimura <tsujimura@takachiho-sc.co.jp>
2026-04-10 13:08:16 +0900

    260410 UIレイアウト作成その1 ・HomeViewController にてユーザーアイコンの設定を記述 ・UIImageのサイズ変更を行うextensionを作成

---
+++ XSampleApp/UIImage+Resizing.swift
@@ -0,0 +1,30 @@
+//
+//  UIImage+Resizing.swift
+//  XSampleApp
+//
+//  Created by kaihatsu on 2026/04/09.
+//
+
+import UIKit
+
+extension UIImage{
+    
+    func makeCircularImage(image: UIImage,size: CGSize) -> UIImage? {
+        
+        let rect = CGRect(origin: .zero, size: size)
+        
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        let path = UIBezierPath(ovalIn: rect)
+        path.addClip()
+        image.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+        
+    }
+    
+    
+    
+    
+    
+}
