c564fdd697eeaaf45bb6de7953621aabfe3eb3f9
tsujimura <tsujimura@takachiho-sc.co.jp>
2026-04-27 16:45:28 +0900

    260427-2 ダミーユーザー作成のために必要な構造体と ユーザーアイコン画像を追加

---
+++ XSampleApp/Home.swift
@@ -0,0 +1,15 @@
+//
+//  Home.swift
+//  XSampleApp
+//
+//  Created by kaihatsu on 2026/04/27.
+//
+
+import Foundation
+
+struct Home{
+    
+    let imageString: String
+    let name: String
+    let body: String
+}
